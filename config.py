f23d0943e88b7913dfef70dff2afba6a96fd095d
Ian Rae <ian.rae@snhu.edu>
2023-04-04 18:50:26 -0400

    Remove Useless Stuff, Add ALL NBA

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Collecting Data
 
-startingYear = 2004 # First year data will be collected for 
-finalYear = 2022 # Final year data will be collected for
+startingYear = 2023  # First year data will be collected for
+finalYear = 2023  # Final year data will be collected for
 
 # Rating Player
 
@@ -21,14 +21,14 @@ offense = {
 
 # Defensive Statistics
 defense = {
-    "drbWeight": 1,     # Defensive Rebounds
+    "drbWeight": .75,     # Defensive Rebounds
     "stlWeight": 1,     # Steals Per Game
     "blkWeight": 1,     # Blocks Per Game
-    "stlpcWeight": .5,  # Steal Percentage
-    "blkpcWight": .5,   # Block Percentage
-    "dwsWeight": 3,     # Defensive Win Shares
-    "dbpmWeight": 3.5,  # Defensive Box Plus Minus
+    "stlpcWeight": 1.5,  # Steal Percentage
+    "blkpcWight": 1.5,   # Block Percentage
+    "dwsWeight": 2.5,     # Defensive Win Shares
+    "dbpmWeight": 3,  # Defensive Box Plus Minus
 }
 
-MAX_RATING = 97         # Rating of the player who had the best season
+MAX_RATING = 98         # Rating of the player who had the best season
 MIN_RATING = 68         # Rating of the player who had the worst season
